811e4b122c93ba1945c2c5eadb94c7fdeffb8cb9
Mikhail Stasyuk <mikhail.stasyuk@gmail.com>
2023-08-20 14:06:14 +0400

    Removed a print statement

--- src/app.py
+++ src/app.py
@@ -1,9 +1,16 @@
+import os 
 import sys
 sys.path.append('src')
 import predict
+import json
 
 def handler(event, context):
     predict.main()
+
+    if os.path.exists('pred.json'):
+        with open('pred.json') as f_in:
+            pred = json.load(f_in)
+            return pred
 
 if __name__ == "__main__":
     handler(None, None)
