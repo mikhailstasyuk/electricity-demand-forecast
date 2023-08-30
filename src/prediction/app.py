e9916eab097b0b3958b460c035a032a6864e084b
Mikhail Stasyuk <mikhail.stasyuk@gmail.com>
2023-08-30 16:37:42 +0400

    Commit

--- src/prediction/app.py
+++ src/prediction/app.py
@@ -10,10 +10,8 @@ import pickle
 # @flow(name='make_prediction')
 def predict(config, model_name):
     stage = "Production"
-    print("Stage:", stage)
     model_uri = f"models:/{model_name}/{stage}"
     print("Model URI:", model_uri)
-    model_uri = "s3://electricity-demand-bucket/mlruns/2e2bcde262ad4520b8da93ba22265361/artifacts/xgb_best"
     model = mlflow.pyfunc.load_model(model_uri=model_uri)
 
     best_run_id = model.metadata.run_id
@@ -28,7 +26,6 @@ def predict(config, model_name):
     return y_pred[0]
     
 def lambda_handler(event, context):
-    # Initialize Hydra
     initialize(version_base=None, config_path="conf/", job_name="lambda_job")
     config = compose(config_name="config.yaml")
     
@@ -37,22 +34,18 @@ def lambda_handler(event, context):
     password = config.data.conn_params.password
     host = config.data.conn_params.host
     port = config.data.conn_params.port
-    print("Using:\n", dbname)
+    
     TRACKING_URI = f'postgresql://{user}:{password}@{host}:{port}/{dbname}'
-    print("Setting URI...", TRACKING_URI)
     mlflow.set_tracking_uri(TRACKING_URI)
     
-    # # mlflow.set_experiment(config.mlflow.experiment_name)
     model_name = config.mlflow.model_name + '-reg'
     
-    try: # Call the prediction function
-        print("Current directory before:", os.getcwd())
+    try: 
         os.chdir("/tmp")
-        print("Current directory after:", os.getcwd())
         pred = predict(config=config, model_name=model_name)
+
         # Return the result as JSON
         result = {'prediction': float(pred)}
-        print(result)
         return result
     
     except Exception as e:
