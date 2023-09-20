bfd4db9b22f2783ac47fbea3c544d0e3db731535
Mikhail Stasyuk <mikhail.stasyuk@gmail.com>
2023-09-20 17:56:25 +0400

    Created a parameters script

---
+++ make_stack_params.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source .env
+
+echo "[
+    {
+        \"ParameterKey\": \"myBucketName\",
+        \"ParameterValue\": \"$S3_BUCKET_NAME\"
+    },
+    {
+        \"ParameterKey\": \"KeyPairName\",
+        \"ParameterValue\": \"$KEY_PAIR_NAME\"
+    },
+    {
+        \"ParameterKey\": \"myInstanceType\",
+        \"ParameterValue\": \"$EC2_INSTANCE_TYPE\"
+    },
+    {
+        \"ParameterKey\": \"myImageId\",
+        \"ParameterValue\": \"$EC2_IMAGE_ID\"
+    },
+    {
+        \"ParameterKey\": \"dbMasterUsername\",
+        \"ParameterValue\": \"$DB_USER\"
+    },
+    {
+        \"ParameterKey\": \"dbMasterUserPassword\",
+        \"ParameterValue\": \"$DB_PASSWORD\"
+    },
+    {
+        \"ParameterKey\": \"myImageUri\",
+        \"ParameterValue\": \"$LAMBDA_IMAGE_URI\"
+    }
+]" > parameters.json
