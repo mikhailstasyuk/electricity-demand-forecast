417d2e1e52827abd918086f0a90a6eda6c1d53f0
Mikhail Stasyuk <mikhail.stasyuk@gmail.com>
2023-09-22 19:23:00 +0400

    Removed a file

--- src/training/get_db_host.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DB_HOST=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`RDSInstanceEndpoint`].OutputValue' --output text)
-export DB_HOST
-
-pipenv run prefect cloud login --key $PREFECT_API_KEY --workspace $PREFECT_WORKSPACE
-
-pipenv run python db_store.py
-pipenv run python main_flow.py
